842a7c4faac3d5bdf08c1384a866f2f712f0ea9c
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-09-20 23:24:35 +0300

    Create price formatter function

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPriceToUSD(price: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
+}
+
