ec5eb428995053ae4e656154f409567bc34791a4
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-09-20 23:20:24 +0300

    Update size of default button component

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
         link: "text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50",
       },
       size: {
-        default: "h-9 px-4 py-2",
+        default: "h-12 px-6 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
@@ -36,7 +36,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
