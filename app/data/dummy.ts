a868bb1e3eac5f86c729754dfa0f6cb774021d82
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-09-22 02:12:37 +0300

    Add example locations

---
+++ app/data/dummy.ts
@@ -0,0 +1,117 @@
+import { StoreLocationProps } from "~/types";
+
+export const locations: StoreLocationProps[] = [
+  {
+    name: 'Book Store 1',
+    address: '123 Main St, New York, NY 10001',
+    phone: '123-456-7890',
+    google_maps_link: 'https://www.google.com/maps/place/123+Main+St,+New+York,+NY+10001'
+  },
+  {
+    name: 'Book Store 2',
+    address: '456 Broadway, New York, NY 10012',
+    phone: '123-456-7890',
+    google_maps_link: 'https://www.google.com/maps/place/456+Broadway,+New+York,+NY+10012'
+  },
+  {
+    name: 'Book Store 3',
+    address: '789 5th Ave, New York, NY 10022',
+    phone: '123-456-7890',
+    google_maps_link: 'https://www.google.com/maps/place/789+5th+Ave,+New+York,+NY+10022'
+  },
+  {
+    name: 'Book Store 4',
+    address: '1600 Pennsylvania Ave NW, Washington, DC 20500',
+    phone: '202-456-1111',
+    google_maps_link: 'https://www.google.com/maps/place/1600+Pennsylvania+Ave+NW,+Washington,+DC+20500'
+  },
+  {
+    name: 'Book Store 5',
+    address: '1 Infinite Loop, Cupertino, CA 95014',
+    phone: '408-996-1010',
+    google_maps_link: 'https://www.google.com/maps/place/1+Infinite+Loop,+Cupertino,+CA+95014'
+  },
+  {
+    name: 'Book Store 6',
+    address: '350 5th Ave, New York, NY 10118',
+    phone: '212-736-3100',
+    google_maps_link: 'https://www.google.com/maps/place/350+5th+Ave,+New+York,+NY+10118'
+  },
+  {
+    name: 'Book Store 7',
+    address: '221B Baker St, Marylebone, London NW1 6XE, UK',
+    phone: '+44 20 7224 3688',
+    google_maps_link: 'https://www.google.com/maps/place/221B+Baker+St,+Marylebone,+London+NW1+6XE,+UK'
+  },
+  {
+    name: 'Book Store 8',
+    address: '1600 Amphitheatre Pkwy, Mountain View, CA 94043',
+    phone: '650-253-0000',
+    google_maps_link: 'https://www.google.com/maps/place/1600+Amphitheatre+Pkwy,+Mountain+View,+CA+94043'
+  },
+  {
+    name: 'Book Store 9',
+    address: '350 5th Ave, New York, NY 10118',
+    phone: '212-736-3100',
+    google_maps_link: 'https://www.google.com/maps/place/350+5th+Ave,+New+York,+NY+10118'
+  },
+  {
+    name: 'Book Store 10',
+    address: '500 S Buena Vista St, Burbank, CA 91521',
+    phone: '818-560-1000',
+    google_maps_link: 'https://www.google.com/maps/place/500+S+Buena+Vista+St,+Burbank,+CA+91521'
+  }
+]
+
+
+export const cities: string[] = [
+  'Los Angeles',
+  'Chicago',
+  'Houston',
+  'Phoenix',
+  'Philadelphia',
+  'San Antonio',
+  'San Diego',
+  'Dallas',
+  'San Jose',
+  'Austin',
+  'Jacksonville',
+  'Fort Worth',
+  'Columbus',
+  'Charlotte',
+  'San Francisco',
+  'Indianapolis',
+  'Seattle',
+  'Denver',
+  'Washington',
+  'Boston',
+  'El Paso',
+  'Nashville',
+  'Detroit',
+  'Oklahoma City',
+  'Portland',
+  'Las Vegas',
+  'Memphis',
+  'Louisville',
+  'Baltimore',
+  'Milwaukee',
+  'Albuquerque',
+  'Tucson',
+  'Fresno',
+  'Sacramento',
+  'Kansas City',
+  'Long Beach',
+  'Mesa',
+  'Atlanta',
+  'Colorado Springs',
+  'Virginia Beach',
+  'Raleigh',
+  'Omaha',
+  'Miami',
+  'Oakland',
+  'Minneapolis',
+  'Tulsa',
+  'Wichita',
+  'New Orleans',
+  'Arlington'
+];
