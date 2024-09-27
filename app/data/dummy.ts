8a5b3f24ce68980602bf26c280a21290cbc8dec3
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-09-28 01:43:41 +0300

    Update book descriptions and add quantity field

--- app/data/dummy.ts
+++ app/data/dummy.ts
@@ -129,7 +129,8 @@ export const books: BookProps[] = [
     discount_percentage: 10,
     stock_quantity: 100,
     cover_image_url: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRPB_n-Tm1DARvOv7YSQDBgfCnDIRpuGeHwAekIw8J3Gu162UPb8UarDHLX1TcoVSC3ntqkzO1zBDINfyqXa0PUOuNpOB2tL60SY34ZxTgxhqenIpCCYxci&usqp=CAE",
-    description: "A novel about government surveillance and mind control under a totalitarian regime."
+    description: "A novel about government surveillance and mind control under a totalitarian regime.",
+    quantity: 1
   },
   {
     id: 2,
@@ -143,7 +144,8 @@ export const books: BookProps[] = [
     price: 15.99,
     stock_quantity: 50,
     cover_image_url: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSMhM81p3_9dhlRmzs5yLu-lNbMerDswAgqUnUbXwwxSRf7Vj8XYYn1JgO_bNBd-T2Nf_gbTt1pwuGWzmeYkWKe95z6H_CDLoa8HayMQ77I4DfDrbOj7G3nmA&usqp=CAE",
-    description: "A gripping story about racial injustice in the Deep South."
+    description: "A gripping story about racial injustice in the Deep South.",
+    quantity: 1
   },
   {
     id: 3,
@@ -157,7 +159,8 @@ export const books: BookProps[] = [
     price: 10.99,
     stock_quantity: 200,
     cover_image_url: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRvKR46d8q7JL0bF3aGFCR-KtkTKCjo_Op9kRnEaXHHEy2RtQ9_rukfuNgOgqDgT_7jOnVry4da1sHGq_-8Axo-nb3E_lEyz0CMcULkmFL1&usqp=CAE",
-    description: "A novel depicting the extravagant lives of the 1920s American elite."
+    description: "A novel depicting the extravagant lives of the 1920s American elite.",
+    quantity: 1
   },
   {
     id: 4,
@@ -171,7 +174,8 @@ export const books: BookProps[] = [
     price: 9.99,
     stock_quantity: 150,
     cover_image_url: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSOGUj_ypQj54mF9VD0TMNaBNYhQoJgGm_XoYlJOyD9zVhz6botKjAhXhNa5UtCEytP24vr0rMor6S8F7ykjqzrmU3TPv5QRSJ5Y_XPMZG9bWJuJz1XINvckg&usqp=CAE",
-    description: "A romantic novel that charts the emotional development of the protagonist Elizabeth Bennet."
+    description: "A romantic novel that charts the emotional development of the protagonist Elizabeth Bennet.",
+    quantity: 1
   },
   {
     id: 5,
@@ -185,7 +189,8 @@ export const books: BookProps[] = [
     price: 14.99,
     stock_quantity: 80,
     cover_image_url: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSvaKFSFatj0uLtyk4KnVKavG0Og83xCpu7p2_n9bYcpSv1JmltHJDQocNV12UM9YLKo8un2Zbzv9Fc0Au8YXM9JdlcsBzNeQz0fNvu6Om9&usqp=CAE",
-    description: "The narrative of Captain Ahab's obsessive quest to kill the white whale, Moby Dick."
+    description: "The narrative of Captain Ahab's obsessive quest to kill the white whale, Moby Dick.",
+    quantity: 1
   },
   {
     id: 6,
@@ -199,7 +204,8 @@ export const books: BookProps[] = [
     price: 19.99,
     stock_quantity: 60,
     cover_image_url: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSDlakVpg13fMM5LdIt8XFVd1zEyGJEq9Bt8vijaG0A48FYpvulVlu6m_6BMSGfQ5g3S0UKC1Ou8FhQ0q_68sUg4mnX-LE57I1wjunmQ6N0&usqp=CAE",
-    description: "A historical novel that intertwines the lives of private and public individuals during the time of the Napoleonic wars."
+    description: "A historical novel that intertwines the lives of private and public individuals during the time of the Napoleonic wars.",
+    quantity: 1
   },
   {
     id: 7,
@@ -213,7 +219,8 @@ export const books: BookProps[] = [
     price: 18.99,
     stock_quantity: 70,
     cover_image_url: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcT6RrqrCc_hRUV-NfefIBpok7Xm1NmlMkgHtHtqYOSmnah7klOoZhhqrTDg6nH2JUTpDMxQHS2EZCpj9yF8T_PR15FiLyhIYwyULy0mmbTMLQrmjasqmm4g&usqp=CAE",
-    description: "A passionate philosophical novel that enters deeply into the ethical debates of God, free will, and morality."
+    description: "A passionate philosophical novel that enters deeply into the ethical debates of God, free will, and morality.",
+    quantity: 1
   },
   {
     id: 8,
@@ -227,7 +234,8 @@ export const books: BookProps[] = [
     price: 11.99,
     stock_quantity: 90,
     cover_image_url: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcR2hsF9tLI2Zt97lzhV1RFth6N9jf9fLj4k2hN0qgH3QLCiYUUPiMCNFOeq2Rae7YM44NbTReDkSqlCPRElCm7ds_m3yu58k-iwuSqh9cpYYyscnXQWR9ouMg&usqp=CAE",
-    description: "A novel that follows the experiences of its eponymous heroine, including her growth to adulthood and love for Mr. Rochester."
+    description: "A novel that follows the experiences of its eponymous heroine, including her growth to adulthood and love for Mr. Rochester.",
+    quantity: 1
   },
   {
     id: 9,
@@ -241,7 +249,8 @@ export const books: BookProps[] = [
     price: 16.99,
     stock_quantity: 85,
     cover_image_url: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQNN8VatZgw1uIYz0arONjyd8LeMff0d5YRP91XiSpUIZy5YKeTzXDq4dvXKYszN_C4sbU1xE2aUvVf-e0KviKLIOjdJ1awqAI_BeT_f84XqRTr-9AQk6aPFA&usqp=CAE",
-    description: "A novel focusing on the mental anguish and moral dilemmas of an impoverished ex-student who kills a pawnbroker."
+    description: "A novel focusing on the mental anguish and moral dilemmas of an impoverished ex-student who kills a pawnbroker.",
+    quantity: 1
   },
   {
     id: 10,
@@ -255,7 +264,8 @@ export const books: BookProps[] = [
     price: 13.99,
     stock_quantity: 120,
     cover_image_url: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTVkqtfhOrrU0BgsiXbJ2NV6a11S6NN_0nVNZAVosS9yucm58rjkTUvgoC0iu7ShbrWamEK3dhQkEvfNSNaqC5zggwbhbxQgWhX9d69BTCRkgaQd0ioE_G36A&usqp=CAE",
-    description: "A story about adolescent Holden Caulfield's disillusionment with the adult world."
+    description: "A story about adolescent Holden Caulfield's disillusionment with the adult world.",
+    quantity: 1
   },
   {
     id: 11,
@@ -269,7 +279,8 @@ export const books: BookProps[] = [
     price: 22.99,
     stock_quantity: 150,
     cover_image_url: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSXQLITvo-7SLOg7-sPzhYohpaVkpWM5zKZj-HoZ_RgVJHFqT9HJKC7dD-YASusJayy_9H8LYM6gjWVmLuLu25WQsGbJF81QDfan3f46m7bE7VO07pOflFr&usqp=CAE",
-    description: "An epic high-fantasy novel that follows the quest to destroy the One Ring."
+    description: "An epic high-fantasy novel that follows the quest to destroy the One Ring.",
+    quantity: 1
   },
   {
     id: 12,
@@ -283,7 +294,8 @@ export const books: BookProps[] = [
     price: 14.99,
     stock_quantity: 95,
     cover_image_url: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSZSJ6r0IFcUKg9ALRoMDxwk_Ng3mMoXCDBlqu-S5roE5Pi1MqR30e1LPv6pEP7xRtLAmPAT0Iinf0OO0Ll5VUHtpO1sTwKz-9x1n1x5og&usqp=CAE",
-    description: "A dystopian novel that explores the dangers of state control and technological advancements."
+    description: "A dystopian novel that explores the dangers of state control and technological advancements.",
+    quantity: 1
   }
 ];
 
