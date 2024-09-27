538392889c43664cfc179780cae22e65b04e4fd2
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-09-28 01:39:54 +0300

    Add animation keyframes for accordion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
     content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
   	extend: {
-      fontFamily: {
+  		fontFamily: {
         sans: [
           '"Inter"',
           "ui-sans-serif",
@@ -16,13 +16,34 @@ export default {
           '"Segoe UI Symbol"',
           '"Noto Color Emoji"',
         ],
-      },
-  		borderRadius: {
+      },	borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-  		colors: {}
+  		colors: {},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
   	}
   },
   plugins: [require("tailwindcss-animate"), require('@tailwindcss/aspect-ratio')],
