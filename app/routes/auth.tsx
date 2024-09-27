ba7c7eac501603e90e23062b5e5222c0f4bbe64d
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-09-28 01:42:40 +0300

    Add background for outlet view

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -3,8 +3,8 @@ import BackgrounDraw from '@assets/images/backgrounds/auth-background.jpeg'
 
 export default function AuthLayout() {
   return (
-    <div className="relative w-full h-dvh flex items-center justify-center overflow-y-auto">
-      <div className='relative z-10'>
+    <div className="relative w-full py-10 flex items-center justify-center overflow-y-auto">
+      <div className='relative z-10 bg-white p-10 shadow-sm rounded-3xl border border-zinc-50'>
         <Outlet />
       </div>
       <div style={{ background: `url('${BackgrounDraw}')` }} className='w-full h-full absolute top-0 left-0 opacity-[0.04]'></div>
