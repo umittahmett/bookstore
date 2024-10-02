42404395c8c29fc627fd65077b15cee4bde1c9ab
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-02 23:44:32 +0300

    Refactor file structure for book-card component

--- app/components/cards/book-card.tsx
+++ app/components/cards/book-card.tsx
@@ -1,29 +1,28 @@
-import React from 'react'
 import { BookProps } from '~/types'
-import { Button } from './ui/button'
-import { Heart, LucideShoppingBag, Plus, ShoppingBag, ShoppingBasket } from 'lucide-react'
+import { Button } from '@components/ui/button'
+import { Heart, ShoppingBag } from 'lucide-react'
 import { formatPriceToUSD } from '@lib/utils'
 
 export const BookCard: React.FC<BookProps> = (book) => {
   return (
     <div className='border border-zinc-100 min-w-72 rounded-lg py-6 bg-white h-full flex flex-col flex-grow'>
-      <div className="h-48 w-full">
+      <a href={`/products/${book._id}`} className="h-48 w-full">
         <img
           alt={book.title}
-          src={book.cover_image_url}
+          src={book.images[0]}
           className="h-full w-auto rounded-md mx-auto"
         />
-      </div>
+      </a>
 
       <div className='mt-6 px-6 flex flex-col relative justify-between h-full'>
         <div>
           <div className='flex items-center justify-between gap-4'>
-            <div>
+            <a href={`/products/${book._id}`}>
               <h2 className='text-zinc-900 line-clamp-2 font-medium text-md text-start leading-5'>
                 {book.title}
               </h2>
               <div className='text-zinc-400 text-xs leading-5 font-light'>by {book.author} </div>
-            </div>
+            </a>
 
             <Button variant="outline" size='icon' className='size-10 flex-shrink-0'>
               <Heart className='size-4 text-zinc-600' />
