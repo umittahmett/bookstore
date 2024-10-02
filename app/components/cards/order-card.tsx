b5e4d3cc8afbbf342b090e2f564b208ef9831175
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-02 23:45:15 +0300

    Refactor OrderCard component to conditionally render return request button for delivered orders

--- app/components/cards/order-card.tsx
+++ app/components/cards/order-card.tsx
@@ -9,20 +9,24 @@ const OrderCard: React.FC<OrderProps> = ({ seller, date, status, product }) => {
     <div className="flex items-start border-t pt-4 gap-4">
       <div className="flex items-center gap-4">
         <div className="size-28 flex-shrink-0 rounded-lg bg-zinc-100 flex p-2 items-center justify-center">
-          <img src={product.cover_image_url} alt={product.title} className="h-full w-fit object-contain" />
+          <img src={product.images[0]} alt={product.title} className="h-full w-fit object-contain" />
         </div>
 
         <div className="flex-grow">
           <h3 className="font-semibold">{product.title}</h3>
           <p className="text-zinc-600 line-clamp-2">{product.description}</p>
-          {getStatus(status)}
+          <div className='mt-2'>
+            {getStatus(status)}
+          </div>
           {status === 'delivered' && <p className="text-sm text-zinc-600">1 ürün {date} tarihinde teslim edilmiştir.</p>}
         </div>
       </div>
 
-      <Button variant="outline" className="ml-auto">
-        İade Talebi
-      </Button>
+      {status === 'delivered' && (
+        <Button variant="outline" className="ml-auto">
+          İade Talebi
+        </Button>
+      )}
     </div>
   );
 };
