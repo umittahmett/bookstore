ebdb6bc0251a574aa38efb0d029a16f9f7972d3d
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-04 16:04:58 +0300

    Add API endpoint to add a product to the cart and implement the functionality in BookCard component

--- app/components/cards/book-card.tsx
+++ app/components/cards/book-card.tsx
@@ -2,8 +2,20 @@ import { BookProps } from '~/types'
 import { Button } from '@components/ui/button'
 import { Heart, ShoppingBag } from 'lucide-react'
 import { formatPriceToUSD } from '@lib/utils'
+import { useFetchAction } from '~/hooks/use-global-submit'
+import { AnimatedCircularProgressBarDemo } from '../ui/progress'
 
 export const BookCard: React.FC<BookProps> = (book) => {
+  const { sendAction, isLoading } = useFetchAction();
+
+  // Add product to carts
+  const handleAddToCart = () => {
+    const formData = new FormData();
+    formData.append('productId', book._id.toString());
+    formData.append('quantity', '1');
+    sendAction(formData, 'post', '/api/cart/add-product');
+  };
+
   return (
     <div className='border border-zinc-100 min-w-72 rounded-lg py-6 bg-white h-full flex flex-col flex-grow'>
       <a href={`/products/${book._id}`} className="h-48 w-full">
@@ -34,8 +46,15 @@ export const BookCard: React.FC<BookProps> = (book) => {
           </div>
         </div>
 
-        <Button className='w-full sticky mt-6'> <ShoppingBag strokeWidth={1} className='text-white size-5' /> Add to cart</Button>
+        <Button onClick={handleAddToCart} className='w-full sticky mt-6'> <ShoppingBag strokeWidth={1} className='text-white size-5' /> Add to cart</Button>
       </div>
     </div>
   )
 }
+
+// {isLoading && (
+//   <div className='fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)] z-50 flex justify-center items-center'>
+//     {/* Yükleme spinner'ı */}
+//     <AnimatedCircularProgressBarDemo isLoading={isLoading} />
+//   </div>
+// )}
