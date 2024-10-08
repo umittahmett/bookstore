dbe7f431e6e753ed9857e0ee7bbc344801d69e33
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-08 16:49:06 +0300

    Refactor book card Remove add to favorites button Add add to cart button

--- app/components/cards/book-card.tsx
+++ app/components/cards/book-card.tsx
@@ -1,9 +1,20 @@
 import { BookProps } from '~/types'
 import { Button } from '@components/ui/button'
-import { Heart, ShoppingBag } from 'lucide-react'
 import { formatPriceToUSD } from '@lib/utils'
+import { useFetchAction } from '@hooks/use-global-submit'
+import { CartPlusIcon } from '@components/ui/vector'
 
 export const BookCard: React.FC<BookProps> = (book) => {
+  const { sendAction } = useFetchAction()
+
+  // Add product to cart
+  const handleAddToCart = () => {
+    const formData = new FormData();
+    formData.append('productId', book._id.toString());
+    formData.append('quantity', '1');
+    sendAction(formData, 'post', '/api/cart/add-product');
+  };
+
   return (
     <div className='border w-full border-zinc-100 rounded-lg py-3 sm:py-6 bg-white h-full flex flex-col flex-grow relative'>
       <a href={`/products/${book._id}`} className="h-48 w-full px-3">
@@ -23,16 +34,13 @@ export const BookCard: React.FC<BookProps> = (book) => {
               </h2>
               <div className='text-zinc-400 text-xs leading-5 font-light'>by {book.author} </div>
             </a>
-
-            <Button variant="outline" size='icon' className='size-10 !absolute top-2 right-2 flex-shrink-0'>
-              <Heart className='size-4 text-zinc-600' />
-            </Button>
           </div>
 
           <div className='text-md font-semibold text-zinc-900 mt-2'>
             {formatPriceToUSD(book.price)}
           </div>
         </div>
+        <Button onClick={handleAddToCart} variant='outline' className='w-full sticky mt-6'> <CartPlusIcon strokeWidth={1} className='text-zinc-800 size-5' /> Add to cart</Button>
       </div>
     </div>
   )
