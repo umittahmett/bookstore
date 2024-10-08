2f46de79f7c68cb1b2583ac8ba221f66fd3fff9c
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-08 15:47:14 +0300

    Remove round of counter input

--- app/components/ui/counter.tsx
+++ app/components/ui/counter.tsx
@@ -7,7 +7,7 @@ const Counter: React.FC<CounterProps> = ({ reduce, increase, count, onChange }) 
   return (
     <div className='flex items-center'>
       <Button disabled={count == 1} size='closeIcon' className='rounded-md !rounded-r-none' type='button' onClick={reduce}><Minus className='size-3 text-white' /></Button>
-      <NumericFormat defaultValue={count} onChange={onChange} value={count} id='founders-count' className='flex h-8 text-center w-12 border border-zinc-200 text-zinc-700 bg-white px-1 text-sm outline-none' allowNegative={false} />
+      <NumericFormat defaultValue={count} onChange={onChange} value={count} id='founders-count' className='flex h-8 text-center rounded-none w-12 border border-zinc-200 text-zinc-700 bg-white px-1 text-sm outline-none' allowNegative={false} />
       <Button size='closeIcon' className='rounded-md !rounded-l-none' type='button' onClick={increase}><Plus className='size-3 text-white' /></Button>
     </div>
   )
