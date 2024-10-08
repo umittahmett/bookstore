3603052fe1916d0722974dfd954cae962c59d9a3
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-08 15:46:01 +0300

    Refactor book card Remove add to cart button Move add to favorites button to top Fix responsive issues

--- app/components/cards/book-card.tsx
+++ app/components/cards/book-card.tsx
@@ -2,23 +2,11 @@ import { BookProps } from '~/types'
 import { Button } from '@components/ui/button'
 import { Heart, ShoppingBag } from 'lucide-react'
 import { formatPriceToUSD } from '@lib/utils'
-import { useFetchAction } from '~/hooks/use-global-submit'
-import { AnimatedCircularProgressBarDemo } from '../ui/progress'
 
 export const BookCard: React.FC<BookProps> = (book) => {
-  const { sendAction, isLoading } = useFetchAction();
-
-  // Add product to carts
-  const handleAddToCart = () => {
-    const formData = new FormData();
-    formData.append('productId', book._id.toString());
-    formData.append('quantity', '1');
-    sendAction(formData, 'post', '/api/cart/add-product');
-  };
-
   return (
-    <div className='border border-zinc-100 min-w-72 rounded-lg py-6 bg-white h-full flex flex-col flex-grow'>
-      <a href={`/products/${book._id}`} className="h-48 w-full">
+    <div className='border w-full border-zinc-100 rounded-lg py-3 sm:py-6 bg-white h-full flex flex-col flex-grow relative'>
+      <a href={`/products/${book._id}`} className="h-48 w-full px-3">
         <img
           alt={book.title}
           src={book.images[0]}
@@ -26,7 +14,7 @@ export const BookCard: React.FC<BookProps> = (book) => {
         />
       </a>
 
-      <div className='mt-6 px-6 flex flex-col relative justify-between h-full'>
+      <div className='mt-6 px-3 sm:px-6 flex flex-col justify-between h-full'>
         <div>
           <div className='flex items-center justify-between gap-4'>
             <a href={`/products/${book._id}`}>
@@ -36,7 +24,7 @@ export const BookCard: React.FC<BookProps> = (book) => {
               <div className='text-zinc-400 text-xs leading-5 font-light'>by {book.author} </div>
             </a>
 
-            <Button variant="outline" size='icon' className='size-10 flex-shrink-0'>
+            <Button variant="outline" size='icon' className='size-10 !absolute top-2 right-2 flex-shrink-0'>
               <Heart className='size-4 text-zinc-600' />
             </Button>
           </div>
@@ -45,16 +33,7 @@ export const BookCard: React.FC<BookProps> = (book) => {
             {formatPriceToUSD(book.price)}
           </div>
         </div>
-
-        <Button onClick={handleAddToCart} className='w-full sticky mt-6'> <ShoppingBag strokeWidth={1} className='text-white size-5' /> Add to cart</Button>
       </div>
     </div>
   )
 }
-
-// {isLoading && (
-//   <div className='fixed top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)] z-50 flex justify-center items-center'>
-//     {/* Yükleme spinner'ı */}
-//     <AnimatedCircularProgressBarDemo isLoading={isLoading} />
-//   </div>
-// )}
