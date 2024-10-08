0ac86fd2fc061b745a7e36ab1d3283b05f3c9ecd
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-09 00:14:15 +0300

    Add md size variant to button component

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -23,6 +23,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-12 px-6 py-2",
+        md: "h-10 px-5",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "size-12",
