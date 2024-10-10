d5701f313dbe42be43daae6174cecae12595c6c9
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-10 16:29:27 +0300

    Create API endpoint for deleting addresses

---
+++ app/routes/api.address.delete.ts
@@ -0,0 +1,25 @@
+import { ActionFunction, json } from "@remix-run/node";
+import { verifyJWT } from "@utils/auth.server";
+import { tokenCookie } from "@utils/cookie";
+import { db } from "@utils/db.server";
+import { JwtPayload } from "jsonwebtoken";
+import { ObjectId } from "mongodb";
+
+export const action: ActionFunction = async ({request }) => {
+  // Check token
+  const token = await tokenCookie.parse(request.headers.get("Cookie"));
+  if (!token) { return json({ error: 'Authorization token required' }, { status: 401 }) }
+  const user = verifyJWT(token) as JwtPayload;
+  if (!user) { return json({ error: 'Invalid or expired token' }, { status: 403 }) }
+
+  // Get form data
+  const formPayload = Object.fromEntries(await request.formData())
+  const addressId = formPayload.addressId as string
+  
+  try {
+    await db.collection('addresses').deleteOne({ _id: new ObjectId(addressId) })
+    return json({ success: true, message: 'Address deleted'})
+  } catch (error) {
+    return json({ error: "Server Error", success: false }, { status: 500 })
+  }
+}
