ce26e394b8846a2cfcc1c6df613a5aff3edbda70
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-10 16:23:53 +0300

    Add global alert dialog atom

--- utils/jotai.ts
+++ utils/jotai.ts
@@ -1,3 +1,5 @@
 import { atom } from 'jotai'
+import { GlobalAlertDialogProps } from '~/types'
 
 export const isLoadingAtom = atom<boolean>(false)
+export const alertDialogAtom = atom<GlobalAlertDialogProps>({open: false, title: '', message: '', onClose: () => {}, onConfirm: () => {}})
