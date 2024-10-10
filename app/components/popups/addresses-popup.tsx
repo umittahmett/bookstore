3ce7e9200961acbb9f3f15170f57eb0baa46eaa2
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-10 16:26:58 +0300

    Disable example addresses in addresses popup

--- app/components/popups/addresses-popup.tsx
+++ app/components/popups/addresses-popup.tsx
@@ -1,5 +1,4 @@
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@components/ui/dialog'
-import { addresses } from '@data/dummy'
 import SavedAddressCard from '@components/cards/saved-address-card'
 
 const AddressesPopup = () => {
@@ -14,9 +13,9 @@ const AddressesPopup = () => {
           <DialogDescription>Select an address</DialogDescription>
         </DialogHeader>
         <div className='space-y-2.5 flex max-h-[75vh] flex-col justify-start overflow-y-auto pr-2.5'>
-          {addresses.map((item, index) => (
+          {/* {addresses.map((item, index) => (
             <SavedAddressCard moreDetailed {...item} key={index} />
-          ))}
+          ))} */}
         </div>
       </DialogContent>
     </Dialog>
