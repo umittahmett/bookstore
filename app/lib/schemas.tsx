194c4367f95b661695add1d21262da536ab32bd8
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-11 23:22:59 +0300

    Move schemas to one file

---
+++ app/lib/schemas.tsx
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+export const addressFormSchema = z.object({
+  name: z.string().min(2, { message: "Name must be at least 2 characters." }),
+  firstName: z.string().min(2, { message: "First name must be at least 2 characters." }),
+  lastName: z.string().min(2, { message: "Last name must be at least 2 characters." }),
+  address: z.string().min(5, { message: "Address must be at least 5 characters." }),
+  addressLine2: z.string().optional(),
+  country: z.string().min(2, { message: "Country must be at least 2 characters." }),
+  city: z.string().min(2, { message: "City must be at least 2 characters." }),
+  zip: z.string().min(5, { message: "ZIP code must be at least 5 characters." }),
+  phone: z.string().min(10, { message: "Phone number must be at least 10 characters." }),
+  additionalInfo: z.string().optional(),
+})
+
+
+export const membershipSchema = z.object({
+  fullName: z.string().min(2, "Fullname must be at least 2 characters"),
+  email: z.string().email("Invalid email address"),
+  phone: z.string().regex(/^\d{10}$/, "Phone number must be 10 digits"),
+  birthDay: z.string().min(1, "Please select a birthday"),
+  businessNotifications: z.boolean().optional(),
+})
+
+export const passwordSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  currentPassword: z.string().min(1, "Current password is required"),
+  newPassword: z
+    .string()
+    .min(10, "Password must be at least 10 characters long")
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, "Password must contain at least one uppercase letter, one lowercase letter, and one number"),
+  confirmPassword: z.string().min(1, "Please confirm your new password"),
+}).refine((data) => data.newPassword === data.confirmPassword, {
+  message: "Passwords don't match",
+  path: ["confirmPassword"],
+})
