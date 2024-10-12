251faaaa3f9dc5e26755a771d26d18dce9f94b6a
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-12 21:21:20 +0300

    Create reset password page

---
+++ app/routes/auth.reset-password.tsx
@@ -0,0 +1,9 @@
+import ResetPasswordForm from "@components/forms/reset-pasword-form"
+
+const ResetPassword = () => {
+  return (
+    <ResetPasswordForm className="w-[350px]" title="Reset Password" />
+  )
+}
+
+export default ResetPassword
