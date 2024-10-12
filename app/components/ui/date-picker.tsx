cbd93f3ab668b7e72c91d45a20166808bae509ce
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-12 21:17:40 +0300

    Refactor DatePicker component to use useState hook and add onChange prop

--- app/components/ui/date-picker.tsx
+++ app/components/ui/date-picker.tsx
@@ -1,4 +1,3 @@
-import * as React from "react"
 import { format } from "date-fns"
 import { CalendarDays, ChevronLeft, ChevronRight } from "lucide-react"
 import { cn } from "@lib/utils"
@@ -11,23 +10,43 @@ import {
 } from "@components/ui/popover"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@components/ui/select"
 import { Input } from "@components/ui/input"
+import { useEffect, useState } from "react"
 
-export function DatePicker() {
-  const [date, setDate] = React.useState<Date>()
-  const [year, setYear] = React.useState(new Date().getFullYear())
-  const [month, setMonth] = React.useState(new Date().getMonth())
+interface DatePickerProps {
+  value?: any;
+  onChange?: (date: Date | undefined) => void;
+}
+
+export function DatePicker({ value, onChange }: DatePickerProps) {
+  const dateValue = value && new Date(value)
+  const [date, setDate] = useState<Date | undefined>(value)
+  const [year, setYear] = useState(dateValue?.getFullYear() || new Date().getFullYear())
+  const [month, setMonth] = useState(dateValue?.getMonth() || new Date().getMonth())
 
   const years = Array.from({ length: new Date().getFullYear() - 1900 + 1 }, (_, i) => new Date().getFullYear() - i)
 
-  const handleYearChange = (selectedYear: string) => {
-    setYear(parseInt(selectedYear, 10))
+  const handleYearChange = (increment: number) => {
+    const newYear = year + increment
+    setYear(newYear)
+    updateDate(new Date(newYear, month, 1))
   }
 
-  const handleMonthChange = (increment: number) => {
-    const newDate = new Date(year, month + increment, 1)
-    setYear(newDate.getFullYear())
-    setMonth(newDate.getMonth())
-  }
+  const updateDate = (newDate: Date | undefined) => {
+    setDate(newDate);
+    if (newDate) {
+      setYear(newDate.getFullYear());
+      setMonth(newDate.getMonth());
+    }
+    onChange?.(newDate);
+  };
+
+  useEffect(() => {
+    if (dateValue) {
+      setDate(dateValue)
+      setYear(dateValue.getFullYear())
+      setMonth(dateValue.getMonth())
+    }
+  }, [value])
 
   return (
     <Popover>
@@ -38,17 +57,22 @@ export function DatePicker() {
               "w-full justify-start text-left font-normal cursor-pointer hover:bg-zinc-50 duration-200 pl-12",
               !date && "text-muted-foreground"
             )}
-            value={date ? format(date, "PPP") : 'Pick a date'}
+            value={date ? format(date, "PPP") : "Pick a date"}
+            readOnly
           />
           <CalendarDays className="absolute left-4 top-1/2 transform size-5 text-zinc-500 -translate-y-1/2" />
         </div>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
+      <PopoverContent className="w-auto h-96 p-0">
         <div className="flex items-center justify-between p-2">
-          <Button variant="outline" size="closeIcon" onClick={() => handleMonthChange(-1)}>
+          <Button size="closeIcon" type="button" variant="outline" onClick={() => handleYearChange(-1)}>
             <ChevronLeft className="h-4 w-4" />
           </Button>
-          <Select value={year.toString()} onValueChange={handleYearChange}>
+          <Select value={year.toString()} onValueChange={(selectedYear) => {
+            const newYear = parseInt(selectedYear, 10)
+            setYear(newYear)
+            updateDate(new Date(newYear, month, 1))
+          }}>
             <SelectTrigger className="w-[100px] h-8">
               <SelectValue>{year}</SelectValue>
             </SelectTrigger>
@@ -60,16 +84,16 @@ export function DatePicker() {
               ))}
             </SelectContent>
           </Select>
-          <Button variant="outline" size="closeIcon" onClick={() => handleMonthChange(1)}>
+          <Button size="closeIcon" type="button" variant="outline" onClick={() => handleYearChange(1)}>
             <ChevronRight className="h-4 w-4" />
           </Button>
         </div>
         <Calendar
           mode="single"
           selected={date}
-          onSelect={setDate}
+          onSelect={(newDate: any) => updateDate(newDate || undefined)}
           month={new Date(year, month)}
-          onMonthChange={(newMonth) => {
+          onMonthChange={(newMonth: any) => {
             setYear(newMonth.getFullYear())
             setMonth(newMonth.getMonth())
           }}
