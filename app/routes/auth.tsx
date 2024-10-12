b78b2142be73bd8a28e8d55d6a0ea4ac502fba6c
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-12 21:18:23 +0300

    Add redirect for logged-in users

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,5 +1,9 @@
-import { Outlet } from '@remix-run/react'
+import { Outlet, redirect } from '@remix-run/react'
 import BackgrounDraw from '@assets/images/backgrounds/auth-background.jpeg'
+import { verifyJWT } from '@utils/auth.server';
+import { tokenCookie } from '@utils/cookie';
+import { JwtPayload } from 'jsonwebtoken';
+import { LoaderFunction } from '@remix-run/node';
 
 export default function AuthLayout() {
   return (
@@ -11,3 +15,17 @@ export default function AuthLayout() {
     </div>
   )
 }
+
+export const loader: LoaderFunction = async ({ request }) => {
+  try {
+    // Redirect if user is already logged in
+    const token = await tokenCookie.parse(request.headers.get("Cookie"));
+    const user = token && verifyJWT(token) as JwtPayload
+    if (user) { return redirect("/") }
+
+    return null
+  } catch (error) {
+    console.error("Loader sırasında bir hata oluştu:", error);
+    throw new Error("Sunucu hatası: Kullanıcı doğrulama işlemi başarısız.");
+  }
+};
