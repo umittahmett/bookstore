601fa949a9ab9b261354be7566b58ea37f2a137b
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-14 14:29:39 +0300

    Refactor addresses popup

--- app/components/popups/addresses-popup.tsx
+++ app/components/popups/addresses-popup.tsx
@@ -1,11 +1,13 @@
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@components/ui/dialog'
 import SavedAddressCard from '@components/cards/saved-address-card'
+import { AddressesPopupProps } from '~/types'
+import { Button } from '../ui/button'
 
-const AddressesPopup = () => {
+const AddressesPopup: React.FC<AddressesPopupProps> = ({ addresses, setSelectedAddress }) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <button className='text-sm font-light mb-6 mt-2 ml-1 duration-200 text-zinc-600 hover:text-black'>View all</button>
+        <Button className='mt-2' size="sm">Change</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-3xl">
         <DialogHeader>
@@ -13,9 +15,16 @@ const AddressesPopup = () => {
           <DialogDescription>Select an address</DialogDescription>
         </DialogHeader>
         <div className='space-y-2.5 flex max-h-[75vh] flex-col justify-start overflow-y-auto pr-2.5'>
-          {/* {addresses.map((item, index) => (
-            <SavedAddressCard moreDetailed {...item} key={index} />
-          ))} */}
+          {addresses.length > 0 ?
+            addresses?.map((item, index) => (
+              <SavedAddressCard onClick={() => setSelectedAddress(item)} moreDetailed {...item} key={index} />
+            ))
+            :
+            <p className='text-zinc-500 py-10 text-sm text-center'>No addresses found</p>
+          }
+          <a href="/profile/addresses/new" className='ml-auto'>
+            <Button size='sm'>Add new</Button>
+          </a>
         </div>
       </DialogContent>
     </Dialog>
