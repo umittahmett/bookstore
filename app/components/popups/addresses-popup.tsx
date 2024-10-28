8e6c2192d68e7b85637724f0c1bdbfd37525bf39
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-10-29 01:28:11 +0300

    Update submission function in addresses popup

--- app/components/popups/addresses-popup.tsx
+++ app/components/popups/addresses-popup.tsx
@@ -5,14 +5,16 @@ import { Button } from '../ui/button'
 import { useState } from 'react'
 import AddressDetailForm from '../forms/address-detail-form'
 import { Plus } from 'lucide-react'
+import { set } from 'date-fns'
 
 const AddressesPopup: React.FC<AddressesPopupProps> = ({ addresses, setSelectedAddress }) => {
   const [showAddressForm, setShowAddressForm] = useState<boolean>(false)
   const title = showAddressForm ? 'Add new address' : 'Saved Addresses'
   const desctiption = showAddressForm ? 'Fill the form below to add a new address' : 'Select an address'
+  const [open, setIsOpen] = useState<boolean>(false)
 
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
         <Button className='mt-2' size="sm">Change</Button>
       </DialogTrigger>
@@ -25,7 +27,10 @@ const AddressesPopup: React.FC<AddressesPopupProps> = ({ addresses, setSelectedA
           <div className='space-y-2.5 flex max-h-[75vh] flex-col justify-start overflow-y-auto pr-2.5'>
             {addresses.length > 0 ?
               addresses?.map((item, index) => (
-                <SavedAddressCard onClick={() => setSelectedAddress(item)} moreDetailed {...item} key={index} />
+                <SavedAddressCard onClick={() => {
+                  setSelectedAddress(item)
+                  setIsOpen(false)
+                }} moreDetailed {...item} key={index} />
               ))
               :
               <p className='text-zinc-500 py-10 text-sm text-center'>No addresses found</p>
@@ -33,7 +38,7 @@ const AddressesPopup: React.FC<AddressesPopupProps> = ({ addresses, setSelectedA
             <Button className='absolute bottom-5 right-5 pl-3.5' onClick={() => setShowAddressForm(!showAddressForm)} ><Plus className='size-5' />Add new</Button>
           </div>
           :
-          <AddressDetailForm successFunction={() => window.location.reload()} redirectTo='/checkout' action='add' />
+          <AddressDetailForm successFunction={() => setShowAddressForm(false)} redirectTo='/checkout' action='add' />
         }
       </DialogContent>
     </Dialog>
