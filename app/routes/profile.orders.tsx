b2b431d7a6c4c21f82f9f42cb166b267e51f73da
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-11-08 16:49:25 +0300

    Implement orders loader function and display user orders

--- app/routes/profile.orders.tsx
+++ app/routes/profile.orders.tsx
@@ -2,13 +2,23 @@ import { Search, ChevronDown } from "lucide-react"
 import { Button } from "@components/ui/button"
 import { Input } from "@components/ui/input"
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@components/ui/tabs"
-import { useEffect, useState } from "react"
-import { deliveredOrders } from "~/data/dummy"
-import OrderCard from "@components/cards/order-card"
+import { useState } from "react"
+import { json, LoaderFunction, redirect } from "@remix-run/node"
+import { tokenCookie } from "@utils/cookie"
+import { verifyJWT } from "@utils/auth.server"
+import { JwtPayload } from "jsonwebtoken"
+import { connectToDatabase } from "@utils/db.server"
+import { useLoaderData } from "@remix-run/react"
+import OrderCard from "~/components/cards/order-card"
+import { OrderProps } from "~/types"
 
 export default function Orders() {
   const [keyword, setKeyword] = useState("")
   const [selectedTab, setSelectedTab] = useState("all")
+
+  const loaderData = useLoaderData<typeof loader>()
+  const { orders } = loaderData
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Siparişlerim</h1>
@@ -38,12 +48,46 @@ export default function Orders() {
         </TabsList>
         <TabsContent value="all">
           <div className="space-y-4">
-            {/* {deliveredOrders.map((order, index) => (
-              <OrderCard key={index} {...order} />
-            ))} */}
+            {orders.map((order: OrderProps, index: number) => (
+              <div key={index}>
+                {order.products.map((product: any, productIndex) => (
+                  <OrderCard key={productIndex} {...product} />
+                ))}
+              </div>
+            ))}
           </div>
         </TabsContent>
       </Tabs>
     </div>
   )
 }
+
+
+// Loader function to orders
+export const loader: LoaderFunction = async ({ request }) => {
+  try {
+    // Check token
+    const token = await tokenCookie.parse(request.headers.get("Cookie"));
+    if (!token) { return redirect("/auth/login") }
+    const user = verifyJWT(token) as JwtPayload;
+    if (!user) { return redirect("/auth/login") }
+
+    // Get user orders
+    const { db } = await connectToDatabase()
+    const ordersData = await db.collection('orders').find({ userId: user.id }).toArray()
+
+    const orders = await Promise.all(ordersData.map(async (order: any) => {
+      const products = await db.collection('products').find({ _id: { $in: order.products } }).toArray()
+      return { ...order, products }
+    }
+    ))
+
+    console.log(orders);
+
+
+    return json({ orders })
+  } catch (error) {
+    console.error("Loader sırasında bir hata oluştu:", error);
+    throw new Error("Sunucu hatası: Kullanıcı doğrulama işlemi başarısız.");
+  }
+};
